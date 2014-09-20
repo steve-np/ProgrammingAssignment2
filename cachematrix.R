cd5d20563d8f95814221e5e80329447ce12f0ae1
steve-np <steve_sf@yahoo.com>
2014-09-19 23:09:41 -0400

    cachematrix first effort

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function makes a special matrix that can cache its inverse.
+## I want this to test if this is a "square" matrix, and abort if it is not, but I haven't figured that out yet.
+## Nice article on singular, or non-invertible matrices here: 
+##     http://stackoverflow.com/questions/13145948/how-to-find-out-if-a-matrix-is-singular
+## Also, check out this: http://en.wikipedia.org/wiki/Invertible_matrix
+## 1.  set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the inverse
+## 4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+        
 }
 
+## This function tests to see if the inverse has already been calculated, and fetches it. If it doesn't exist,
+## it calculates the inverse of the matrix in makeCacheMatrix. 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) { 
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        ## inv <- solve(data, ...) $*$ (data, ...)
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
